dad32c8d962f6cdcd24277b39a95f0ebd10282a4
Abdelrahman Afifi <a.afifi5299@gmail.com>
2019-07-12 07:12:49 +1000

    NEW: Add registeration and login functionality.

--- web/public/app.js
+++ web/public/app.js
@@ -1,6 +1,7 @@
 $("#navbar").load("navbar.html");
 $("#footer").load("footer.html");
 
+const users = JSON.parse(localStorage.getItem('users')) || [];
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
 devices.forEach( device => {
@@ -17,10 +18,48 @@ $('#add-device').on('click', () => {
 	const name = $("#name").val();
 	devices.push({user, name});
 	localStorage.setItem('devices', JSON.stringify(devices));
-	location.href = "/"
+	location.href = "/";
 });
 
 $('#send-command').on('click', () => {
 	const command = $('#command').val();
 	console.log(`command is: ${command}`);
 });
+
+$('#register').on('click', () => {
+	const username = $('#username').val();
+	const password = $('#password').val();
+	const confirm = $('#confirm-password').val();
+	const exists = users.find(user => user.name === username);
+	if (exists) {
+		$('#message').text('Username already exist.');
+		$('#message').show();
+	} else {
+		if (!(password === confirm)) {
+			$('#message').text("Password doesn't match.");
+			$('#message').show();
+		} else {
+			users.push({name: username, password: password});
+			localStorage.setItem('users', JSON.stringify(users));
+			location.href = '/login';
+		}
+	}
+});
+
+$('#login').on('click', () => {
+	const username = $('#username').val();
+	const password = $('#password').val();
+	const exists = users.find(user => user.name === username);
+	if (!exists || !(exists.password === password)) {
+		$('#message').text("Wrong credentials");
+		$('#message').show();
+	} else {
+		localStorage.setItem('isAuthenticated', true);
+		location.href = '/';
+	}
+});
+
+const logout = () => {
+	localStorage.removeItem('isAuthenticated');
+	location.href = "/login";
+}
