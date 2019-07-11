f5039935168411dbd01de2aa72bde6be31b52fd2
Abdelrahman Afifi <a.afifi5299@gmail.com>
2019-07-11 19:15:31 +1000

    NEW: Add 404 error page.

--- web/server.js
+++ web/server.js
@@ -11,6 +11,26 @@ app.get('/', (req, res) => {
 	res.sendFile(`${base}/device-list.html`);
 });
 
+app.get('/register-device', (req, res) => {
+	res.sendFile(`${base}/register-device.html`);
+});
+
+app.get('/register-device', (req, res) => {
+	res.sendFile(`${base}/register-device.html`);
+});
+
+app.get('/send-command', (req, res) => {
+	res.sendFile(`${base}/send-command.html`);
+});
+
+app.get('/about-me', (req, res) => {
+	res.sendFile(`${base}/about-me.html`);
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(`${base}/404.html`);
+});
+
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
