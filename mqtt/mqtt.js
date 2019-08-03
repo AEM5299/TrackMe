6fd86b80672ec3a2f80dfc1d5bb94a073b1bc940
Abdelrahman Afifi <a.afifi5299@gmail.com>
2019-08-03 17:01:50 +1000

    NEW: mqtt server now connected to db

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -1,10 +1,15 @@
 const mqtt = require('mqtt');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const rand = require('random-int');
+const randomCoordinates = require('random-coordinates');
+const Device = require('./models/device');
 
 const app = express();
 const { URL, USERNAME, PASSWORD } = process.env;
 const port = process.env.PORT || 5001;
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -17,6 +22,14 @@ app.use(function(req, res, next) {
 	next();
 });
 
+function returnJSON(success, message, data) {
+	return {
+		success: success,
+		message: message,
+		data: data
+	};
+}
+
 const client = mqtt.connect(URL, {
 	username: USERNAME,
 	password: PASSWORD
@@ -24,13 +37,81 @@ const client = mqtt.connect(URL, {
 
 client.on('connect', () => {
 	console.log('MQTT Connected');
+	client.subscribe('/sensorData');
 });
 
+client.on('message', (topic, message) => {
+	if(topic == '/sensorData') {
+		const data = JSON.parse(message.toString());
+		console.log(data);
+
+		Device.findOne({"name": data.deviceId}, (err, device) => {
+			if(err) console.log(err);
+			const { sensorData } = device;
+			const { ts, loc, temp } = data;
+
+			sensorData.push({ts, loc, temp});
+			device.sensorData = sensorData;
+
+			device.save(err => {
+				if(err) console.log(err);
+			});
+		});
+
+	}
+});
+
+/**
+* @api {put} /sensor-data FakeSensorData Create a set of fake data and puplish it to the mqtt server
+* @apiVersion 1.0.0
+* @apiGroup Device
+* @apiParam {String} deviceId Device's name
+* @apiSuccessExample {json} Success-Response:
+* {
+* 	"success": true,
+* 	"message": "Fake data sent",
+* 	"data": {
+* 		deviceId: 'Mary's iPhone',
+* 		ts: 1564267719763,
+* 		loc: {
+* 			lat: '-51.09538',
+* 			lon: '142.02451'
+* 		},
+* 		temp: 30
+* 	}
+* }
+*/
+app.put('/sensor-data', (req, res) => {
+	const {deviceId} = req.body;
+	const [lat, lon] = randomCoordinates().split(", ");
+	const ts = new Date().getTime();
+	const loc = {lat, lon};
+	const temp = rand(20, 50);
+	const topic = '/sensorData';
+	const message = JSON.stringify({deviceId, ts, loc, temp});
+
+	client.publish(topic, message, () => {
+		return res.json(returnJSON(true, "Fake data sent", {deviceId, ts, loc, temp}));
+	});
+});
+
+/**
+* @api {post} /send-command SendCommand Sends a command through the mqtt server
+* @apiVersion 1.0.0
+* @apiGroup Device
+* @apiParam {String} deviceId Device's name
+* @apiParam {String} command The command
+* @apiSuccessExample {json} Success-Response:
+* {
+* 	"success": true,
+* 	"message": "Command published"
+* }
+*/
 app.post('/send-command', (req, res) => {
 	const { deviceId, command } = req.body;
 	const topic = `/command/${deviceId}`;
 	client.publish(topic, command, () => {
-		res.send('published new message');
+		return res.json(returnJSON(true, "Command published"));
 	});
 });
 
